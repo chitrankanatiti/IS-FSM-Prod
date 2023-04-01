d7dc524d4b4c2d10826a0f3bf665f8515808f324
CHITRANKANA BHOWMIK <61222351+chitrankanatiti@users.noreply.github.com>
2023-04-02 01:57:36 +0530

    Delete engineerprofile directory

--- engineerprofile/repository/EngineerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hdsoft.fsm.engineerprofile.repository;
-
-import com.hdsoft.fsm.engineerprofile.model.EngineerModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EngineerRepository extends JpaRepository<EngineerModel, Long>{
-}
